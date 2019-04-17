fe880d73e4ee9be86bd438345c30cddcf770f2df
Sakib Khan Inan <46783803+skinan@users.noreply.github.com>
2019-04-18 00:36:17 +0600

    Update 1119A.cpp

--- 1119A.cpp
+++ 1119A.cpp
@@ -8,29 +8,25 @@ using namespace std;
 
 int main()
 {
-   
     int x;
     int i, j, n;
-    
+
     scanf("%d", &x);
-    
+
     vector<int> arr(x);
-
-
     for(i  = 0; i < x; i++)
     {
         scanf("%d", &arr[i]);
-        
     }
-
     n = x - 1;
     j = n;
+    
+    //Comparing and calculating results below in while loop.
     while(arr[0] == arr[n - j] && arr[0] == arr[j] )
     {
         j = j - 1;
-        
     }
+    // j is final answer.
     printf("%d", j);
-
-   
+    return 0;
 }
